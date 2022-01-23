d534e423624fe4d146d00d70b9828d50b99d7fc9
Jessica <85376230+criminy-git@users.noreply.github.com>
2022-01-23 18:59:46 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
+const url1 = "https://api.adviceslip.com/advice";
 
-const url1 = "https://api.adviceslip.com/advice"
+// const adviceCards = document.getElementbyId("output");
+
+// const promises = [];
+// for (let i=1; i<=10; i++){
+//     const url1 = "https://api.adviceslip.com/advice";
+//     promises.push(fetch(url)
+//                   .then(res => res.json()));
+// }
+// Promise.all(promises)
+//     .then(results => {
+//     const allAdvice = slip.map(data => ({
+        
+        
+// }
 
 document.getElementById("btn").addEventListener("click", () => {
+    for (let i=1; i<=10; i++){
     fetch(url1)
     .then (res => res.json())
     .then (data => {
         console.log(data)
          document.getElementById("output").innerHTML =
         `<p>${data.slip.advice}</p>`
+    }
     })
 })
 
